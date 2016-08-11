b0ddbfa3976e5aaa6ee473eb45e45e796ff9a7c0
jon-is-learning <jonathan.garrett@gmail.com>
2016-08-11 13:17:34 -0700

    Complete songQueue and songModel test specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,27 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    // when add is called, call playFirst
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.shift();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function(model) {
+      this.remove(model);
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
 
 });
